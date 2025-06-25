a536a8522e07a1234f0d359bbd455fc28630c144
Vida <quiniavidamarie30@gmail.com>
2025-06-25 23:16:17 +0800

    added and desgined a home page using mostly js

--- src/home.js
+++ src/home.js
@@ -9,6 +9,30 @@ export function generateHome(){
     content.style.width = '100vw';
     content.style.height = '100vh';
     
+    const titleBox = document.createElement("div");
+    titleBox.setAttribute("style", 
+        "width: 100vw; height: 30vh; background-color: rgba(0,0,0,0.5); position: relative; top: 50vh; display:flex; box-shadow: 0 0 10px rgba(0,0,0,0.5)"
+    );
+
+    content.appendChild(titleBox);
+
+    const titleStyle = "font-size: 90px; font-weight:bold; color: rgba(130, 139, 123, 1); font-style: italic; margin-left: 50px; display:flex; justify-content:center; align-items: center";
+
+    const title1 = document.createElement("h1");
+    title1.setAttribute("style", titleStyle)
+    title1.textContent = `The Grind`;
+
+    const title2 = document.createElement("h1");
+    title2.setAttribute("style", titleStyle);
+    title2.textContent = `Starts Here`;
+
+    const titleContainer = document.createElement("div");
+    titleContainer.setAttribute("style", "display:flex; flex-direction: column; justify-content: center; align-items: start;");
+
+    titleContainer.appendChild(title1);
+    titleContainer.appendChild(title2);
+
+    titleBox.appendChild(titleContainer);
 };
 
 
--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
 import "./style.css";
 import { generateHome } from "./home";
+
+const homeButton = document.getElementById("home");
+const menuButton = document.getElementById("menu");
+const aboutButton = document.getElementById("about");
+
+homeButton.addEventListener("click", ()=>{
+    generateHome();
+})
+
 generateHome();
